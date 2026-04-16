1956f68f9fcfcfb1c85912293b20727a6a2097b1
srlokesho45-gif <srlokesho45@gmail.com>
2026-04-16 18:14:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Change movie name dynamically
+const movies = document.querySelectorAll(".title");
+
+movies[0].innerText = "Leo";
+movies[1].innerText = "Vikram";
+
+// Change rating
+const ratings = document.querySelectorAll(".rating");
+
+ratings[0].innerText = "Rating: 9.3";
+ratings[1].innerText = "Rating: 8.8";
